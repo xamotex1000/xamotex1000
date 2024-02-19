9b807a3af0fee4f9c82a5a24b5140d6a9da3d6d2
xamotex1000 <140911479+xamotex1000@users.noreply.github.com>
2024-02-19 11:06:12 -0600

    Update automation.py

--- automation.py
+++ automation.py
@@ -29,10 +29,61 @@ def change_file_contents(file_path, new_content):
 stats = Stats()
 file_path = "./GHRMStats.html"
 new_content = f'''
-<html>
-<head>
-    <meta http-equiv="refresh" content="0; url=https://github-readme-stats.vercel.app/api?username={stats.username}&theme={stats.theme}&show_icons={stats.show_icons}&hide_border={stats.hide_border}&count_private={stats.count_private}">
-</head>
-</html>
+# Hi, I'm Milo! 👋 
+
+I'm Milo, a high school student with a passion for software engineering and creating innovative projects. Here's a bit more about me:
+
+### About Me
+
+ - 🎓 Currently in high school, doing highschool things. 
+- 💻 Interested in software engineering, I like to try different programming languages and technologies.
+
+### Experience
+
+I work best in a Linux environment (preferably using a bash shell)
+
+|<b>Language</b>|<b>Confidence</b>|
+|-|-|
+|<img src="https://img.shields.io/badge/JavaScript%20-%20%230050b1?style=flat&logo=Node.js" height=32></img>|9.5/10, My Main Language|
+|<img src="https://img.shields.io/badge/C%20Sharp%20%20%20-%20%230050b1?style=flat&logo=C%23&logoColor=%23900090" alt="C#" height=32></img>|7/10, Still learning|
+|<img src="https://img.shields.io/badge/Python%20-%20%230050b1?style=flat&logo=Python&logoColor=%23ffff00" alt="Python" height=32></img>|6/10, I know some stuff|
+|<img src="https://img.shields.io/badge/GNU%20Bash%20-%20%230050b1?style=flat&logo=GNU%20Bash" alt="Bash" height=32></img>|5/10, I use it randomly|
+|<img src="https://img.shields.io/badge/C%20-%20%230050b1?style=flat&logo=C" alt="C" height=32></img>|2/10, It's hard ok?|
+|<img src="https://img.shields.io/badge/PHP%20-%20%230050b1?style=flat&logo=Php&logoColor=%23000090" alt="PHP" height=32></img>|1/10, I hate this language|
+
+<details><summary>My Journey Into Programming</summary>
+
+I embarked on my programming journey at the age of 13 with a dream of designing my own game. Starting in Unity, I quickly encountered the challenge of C# but found relief during my freshman year of high school when I enrolled in a computer programming class. There, I learned JavaScript essentials and was captivated by the endless possibilities. Experimenting with small projects, I later ventured into HTML to create a file-sharing website for friends.
+
+My uncle, a computer science graduate, introduced me to the performance benefits of Linux, prompting me to install Ubuntu and delve into Bash scripting for automation in Unix-based environments.
+
+Dabbling in C#, I ventured into modding a game I was passionate about, finding a warm and welcoming community of moderators. Despite encountering burnout during the final stretch of a mod, I took a break but remained inspired by the prospect of working at Valve, branching from the many positive aspects of Valve's work environment.
+
+Now, I'm dedicated to expanding my skills, aiming for a computer engineering degree, and hoping to pursue a career at Valve in my early 20s.
+</details>
+
+### Contact Me
+
+You can reach out to me here:
+<br>
+<a href=https://discord.com/users/450702721763508235>
+<img src=https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a69f118df70ad7828d4_icon_clyde_blurple_RGB.svg alt="Discord Profile" width=50 height=50>
+</a>
+<br>
+I don't check any other social media dms, but whether you have questions, collaboration opportunities, or just want to chat, my Friend Requests are open.
+
+### Collaboration
+
+I'm open to collaborating with other developers on projects. Whether it's pair programming, contributing to open-source projects, or joint project development, I'm down for whatever.
+
+### Future Plans
+
+My main goal is to acquire enough skills to get hired at Valve. Other than that I want to make my dream game before I turn 30. No other long-term goals or aspirations within the field of software engineering at the moment.
+
+### Projects
+
+I enjoy building various projects to refine my skills and explore new technologies. Due to low motivation, tons of my projects were abandoned half way through development and never saw the light of day, but I'll finish them I swear!!! also im currently more into application building than game building.
+
+![xamotex1000's GitHub Stats](https://github-readme-stats.vercel.app/api?username={stats.username}&theme={stats.theme}&show_icons={stats.show_icons}&hide_border={stats.hide_border}&count_private={stats.count_private})
 '''
 change_file_contents(file_path, new_content)
