e45d57b10370b563061babdfa846fd208bcf0b6a
xamotex1000 <140911479+xamotex1000@users.noreply.github.com>
2024-02-19 08:27:18 -0600

    Update automation.py

--- automation.py
+++ automation.py
@@ -1,3 +1,11 @@
+class Stats
+    def __init__(self):
+        self.username = "xamotex1000"
+        self.theme = "cobalt"
+        self.show_icons = "true"
+        self.hide_border = "false"
+        self.count_private = "true"
+
 def change_file_contents(file_path, new_content):
     try:
         with open(file_path, 'w') as file:
@@ -8,10 +16,11 @@ def change_file_contents(file_path, new_content):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-file_path = '''
+file_path = "./GHRMStats.html"
+new_content = f'''
 <html>
 <head>
-    <meta http-equiv="refresh" url=">
+    <meta http-equiv="refresh" url="https://github-readme-stats.vercel.app/api?username={Stats.username}&theme={Stats.theme}&show_icons={Stats.show_icons}&hide_border={Stats.hide_border}&count_private={Stats.count_private}">
 </head>
 </html>
 '''
